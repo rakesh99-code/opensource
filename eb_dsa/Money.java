d7f3b13865ca9f69db842bb48be64190bb3c0ad6
rakesh99-code <rakeshraodeshineni@gmail.com>
2024-11-20 21:22:44 +0530

    Create Money.java

---
+++ eb_dsa/Money.java
@@ -0,0 +1,15 @@
+mport java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc = new Scanner(System.in);
+	   int x = sc.nextInt();
+	   int y = sc.nextInt();
+	   int tot = 5000*x + 9800*y;
+	   System.out.println(tot);
+	}
+}
